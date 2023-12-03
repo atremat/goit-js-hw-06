e73fc83b033b6311cd66a18a4ce05a7bd2820a05
Artem <atremat@gmail.com>
2023-12-03 19:38:13 +0200

    task 2 done

---
+++ js/task-2.js
@@ -0,0 +1,28 @@
+class Storage {
+    #items;
+
+    constructor(arrayOfItems) {
+        this.#items = arrayOfItems;
+    }
+
+    getItems() {
+        return this.#items;
+    }
+
+    addItem(newItem) {
+        this.#items.push(newItem);
+    }
+
+    removeItem(itemToRemove) {
+        this.#items = itemToRemove = this.#items.filter(
+            item => item !== itemToRemove,
+        );
+    }
+}
+
+const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem('Droid');
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem('Prolonger');
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
